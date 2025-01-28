91968d3cbe767a4d01f7b4f7ef380a383444564b
Martin Jirouš <jirousm24@spstrutnov.cz>
2025-01-28 20:39:06 +0100

    animace kanonu

--- main_individuální_projekt.py
+++ main_individuální_projekt.py
@@ -12,7 +12,7 @@ Pohyblive_pozadi = pygame.image.load("Pozadí_pohyblivé.png")
 kanon13= pygame.image.load("kanon_1l3.png")
 kanon23= pygame.image.load("kanon_2l3.png")
 kanon33= pygame.image.load("kanon_3l3.png")
-
+kanon43 = pygame.image.load("kanon_4l3.png")
 
 
 
@@ -24,7 +24,7 @@ kanon33= pygame.image.load("kanon_3l3.png")
 pohyb_pozadí = 0
 rozdil_pozadi = 1920
 výška, šířka = 1080, 1920
-hrac_x = šířka * 1 / 9
+hrac_x = šířka * 1 / 5
 hrac_y = výška / 2
 zivoty = 5
 uhel=0
@@ -47,6 +47,8 @@ pygame.display.set_caption("zkouška")
 pozadi_barva = (0, 255, 255)
 
 while True:
+    nepritel.nabíjení()
+    
     if letadlo.smrt == False:
         nepritel.pohyb_kanonu()
         pohyb_pozadí -= nepritel.rychlost_pozadi
@@ -77,7 +79,16 @@ while True:
     obrazovka.blit(Pohyblive_pozadi, (umisteni_pozadi1, výška - 100))  # vyobrazení pozadí
     obrazovka.blit(Pohyblive_pozadi, (umisteni_pozadi2, výška - 100))
     
-    obrazovka.blit(kanon13,(nepritel.poloha_x,nepritel.poloha_y))
+    
+    if nepritel.vystrel == 1: # animace nabíjení
+        obrazovka.blit(kanon13,(nepritel.poloha_x,nepritel.poloha_y))
+    elif nepritel.vystrel == 2:
+        obrazovka.blit(kanon23,(nepritel.poloha_x,nepritel.poloha_y))
+    elif nepritel.vystrel == 3 :
+        obrazovka.blit(kanon33,(nepritel.poloha_x,nepritel.poloha_y))
+    else:
+        obrazovka.blit(kanon43,(nepritel.poloha_x,nepritel.poloha_y))
+    
     
     otočená_stíhačka = pygame.transform.rotate(stihacka, letadlo.uhel)
     rect = otočená_stíhačka.get_rect(center=(letadlo.x, letadlo.y))
--- nepritel.py
+++ nepritel.py
@@ -1,10 +1,29 @@
 class Nepritel:
-    def __init__(self,rychlost_pozadi,poloha_x,poloha_y,šířka, výška,vystrel):
+    def __init__(self, rychlost_pozadi, poloha_x, poloha_y, šířka, výška, vystrel):
         self.rychlost_pozadi = rychlost_pozadi
         self.sirka = šířka
         self.vyska = výška
         self.poloha_y = poloha_y
-        self.poloha_x = šířka
+        self.poloha_x = poloha_x  # Oprava: Použití správné proměnné
         self.vystrel = vystrel
+
     def pohyb_kanonu(self):
         self.poloha_x -= self.rychlost_pozadi
+    
+    def nabíjení(self):
+        vzdalenost_pred_vystrelem = self.sirka
+        vzdalenost_pred_vystrelem -= self.sirka * 1 / 5
+        vzdalenost_pred_vystrelem = vzdalenost_pred_vystrelem / 3
+        vzdalenost_pred_vystrelem1 = vzdalenost_pred_vystrelem + self.sirka * 1 / 5
+        vzdalenost_pred_vystrelem2 = vzdalenost_pred_vystrelem * 2 + self.sirka * 1 / 5
+        
+        if self.poloha_x > vzdalenost_pred_vystrelem2:
+            self.vystrel = 1
+        elif self.poloha_x > vzdalenost_pred_vystrelem1:
+            self.vystrel = 2
+        elif self.poloha_x > self.sirka * 1 / 5:
+            self.vystrel = 3
+        elif self.poloha_x-10 < self.sirka * 1 / 5 and self.poloha_x+10 > self.sirka * 1 / 5:
+            self.vystrel = 4
+        else:
+            self.vystrel = 0
